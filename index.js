6c8d7272b48e9d10afcb07e8eba83a35a8e0306c
Your Name <you@example.com>
2019-07-02 17:51:55 +0000

    -

--- index.js
+++ index.js
@@ -1,16 +1,73 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'mysql.eclipse-che.svc.cluster.local',
-  user     : 'root',
-  password : 'Migueletes2423',
-  database : 'telecom'
-});
+//include the http and url module
+var http = require('http'),
+    url = require('url'),
+    fs = require('fs');;
 
-connection.connect();
+//create the http server accepting requests to port 3333
+http.createServer(function (req, res) {
+    //get url infomation
+    var urlParts = url.parse(req.url);
+//    console.log(req.url, urlParts);
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+    //direct the request to appropriate function to be processed based on the url pathname
+    switch(urlParts.pathname) {
+        case "/":
+            homepage(req, res);
+            break;
+        case "/read":
+            read(req, res);
+            break;
+        case "/svc/update":
+            update(req, res);
+            break;
+        default:
+            homepage(req,res);
+            break;
+    }
+}).listen(5000);
+console.log("Server running at http://localhost:3333/");
 
-connection.end();
+//functions to process incoming requests
+function homepage(req, res) {
+
+    fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+
+        res.writeHeader(200, {"Content-Type": "text/html"});
+        res.write(html);
+        res.end();
+    });
+}
+
+function read(req, res) {
+    res.end("Hello, there is no data for reading yet.");
+}
+
+function update(req, res) {
+    res.end("Hello, there is no data to update.");
+}
+
+function mysql(){
+
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+    host     : 'mysql.eclipse-che.svc.cluster.local',
+    user     : 'root',
+    password : 'Migueletes2423',
+    database : 'telecom'
+    });
+
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+    });
+
+    connection.end();
+
+}
+
+
