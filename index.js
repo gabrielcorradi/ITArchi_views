b4b89fe8ace49c6583ad8ffcc26ac7bb6f26535a
you <you@example.com>
2019-07-04 14:10:47 +0000

    -

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var http = require('http'),
     url = require('url'),
     fs = require('fs'),
-    mysql = require('mysql2/promise'),
-    util = require('util');
+    mysql = require('mysql2/promise');
+   // util = require('util');
 
 //create the http server accepting requests to port 3333
 http.createServer(function (req, res) {
@@ -61,24 +61,53 @@ function gojs(req, res) {
 
 async function read(req, res) {
 
-    var rta = await mysqlfx();
+query_vistas_0 =    "select id as 'key', class, name, container_id as 'group', content, \
+                    element_id, \
+                    (select class from elements where id=element_id) element_class, \
+                    (select name from elements where id=element_id) element_name \
+                    from views_objects vo where id in ( \
+                        select object_id \
+                        from views_objects_in_view vov \
+                        where view_id in ('4ee92239-acb9-4109-8f1e-c0a64a8f3b30') \
+                    );"
+
+query_vistas_1 =    "select source_object_id, target_object_id, \
+                    (select class from relationships where id=relationship_id) rel_class, \
+                    (select name from relationships where id=relationship_id)rel_name, \
+                    (select documentation from relationships where id=relationship_id) rel_doc \
+                    from views_connections vc where id in ( \
+                        select connection_id from views_connections_in_view vcv  \
+                        where view_id in ('4ee92239-acb9-4109-8f1e-c0a64a8f3b30') \
+                    );
+
+
+
+
+
+
+    query1 = 'select id as "key", name from elements where class = "ApplicationComponent" limit 10;'
+
+    var rta = await mysqlfx(query0);
     res.end(JSON.stringify(rta[0]));
-//    console.log();
+
 }
 
 function update(req, res) {
     res.end("Hello, there is no data to update.");
 }
 
-async function mysqlfx(){
+async function mysqlfx(SQLquery){
+
+//    var host = 'mysql.eclipse-che.svc.cluster.local';
+    var host = '10.75.28.17';
 
     const conn = await mysql.createConnection({
-        host : 'mysql.eclipse-che.svc.cluster.local',
+        host : host,
         user : 'root',
         password : 'Migueletes2423',
         database : 'telecom'});
 
-    var result = await conn.query('select name as "key" from elements where class = "ApplicationComponent" limit 10;');
+    var result = await conn.query(SQLquery);
     //console.log(result[0]);
     conn.end();
     return result;
